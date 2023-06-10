38a148b1d4138a66622c30bc3de44512bd436205
karthikadevops01 <karthikadevops01@gmail.com>
2023-06-10 15:48:07 +0000

    bash script updated

--- automation.sh
+++ automation.sh
@@ -23,7 +23,27 @@ sudo tar -cvf /tmp/karthika-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar /var/log/apa
 echo "--ARCHIVING COMPLETED--"
 echo
 echo "Backing up in S3 Bucket"
+file=/tmp/karthika-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
 aws s3 cp /tmp/karthika-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar s3://upgrad-karthika/karthika-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
 echo "--BACK UP COMPLETED--"
 echo
+echo "Check if inventory file exists, if not create it with header"
+if [[ ! -f /var/www/html/inventory.html ]]; then
+  echo "Log Type        Date Created            Type    Size" > /var/www/html/inventory.html
+fi
+echo "Get log file size and append data to inventory file"
+size=$(sudo stat -c '%s' $file)
+filename=$(basename -- "$file")
+f="${filename%%.*}"
+time="${f:17}"
+echo "httpd-logs        $time        tar     $size" >> /var/www/html/inventory.html
+echo
+echo "Check if cronjob is created, if not create it"
+if [[ ! -f /etc/cron.d/automation ]]; then
+  echo "0 8 * * * bash /root/Automation_Project/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
+if [[ -f /etc/cron.d/automation ]]; then
+  echo "/etc/cron.d/automation already exists"
+fi
+
 
